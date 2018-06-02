b3260b0bc9f24920d0a7309a84ac51b51034ad7b
luizfpinheiro <luiz.pinheiro@cultura.gov.br>
2018-06-02 14:59:04 -0300

    Create/write a json file with infos about user based api.LastJson method

--- examples/test.py
+++ examples/test.py
@@ -4,11 +4,17 @@
 # Use text editor to edit the script and type in valid Instagram username/password
 
 from InstagramAPI import InstagramAPI
+import json 
 
-api = InstagramAPI("login", "password")
+api = InstagramAPI("user", "password")
 if (api.login()):
     api.getSelfUserFeed()  # get self user feed
     print(api.LastJson)  # print last response JSON
+     
+    with open('userInfos.json', 'w') as file: # write log api.LastJson in a json file
+        json.dump(api.LastJson, file) 
+
+    
     print("Login succes!")
 else:
     print("Can't login!")
